9c8590dba166976eb5076d8d8a85c8c06f1f1d3a
PhanTienHuy <huyphan0220@gmail.com>
2023-05-23 21:33:06 +0700

    ud1

---
+++ admin/root/control.php
@@ -0,0 +1,63 @@
+<?php
+    include  '../connect.php';
+    class admin{
+        function get_product($page){
+            global $connect;
+            $no_of_records_per_page = 5;
+            $offset = ($page - 1) * $no_of_records_per_page;
+            $sql = "SELECT * FROM san_pham LIMIT $offset, $no_of_records_per_page";
+            $query=mysqli_query($connect,$sql);
+            if(mysqli_num_rows($query)>0){
+                return $query;
+            }
+        }
+        function get_user(){
+            global $connect;
+            $sql = "SELECT * FROM users ";
+            $query=mysqli_query($connect,$sql);
+            if(mysqli_num_rows($query)>0){
+                return $query;
+            }
+        }
+        function get_contact(){
+            global $connect;
+            $sql = "SELECT * FROM lien_he";
+            $query=mysqli_query($connect,$sql);
+            if(mysqli_num_rows($query)>0){
+                return $query;
+            }
+        }
+        function get_order(){
+            global $connect;
+            $sql = "SELECT * FROM orders";
+            $query=mysqli_query($connect,$sql);
+            if(mysqli_num_rows($query)>0){
+                return $query;
+            }
+        }
+        function get_vnpay(){
+            global $connect;
+            $sql = "SELECT * FROM vn_pay";
+            $query=mysqli_query($connect,$sql);
+            if(mysqli_num_rows($query)>0){
+                return $query;
+            }
+        }
+        function get_orderdetail(){
+            global $connect;
+            $sql = "SELECT * FROM order_product";
+            $query=mysqli_query($connect,$sql);
+            if(mysqli_num_rows($query)>0){
+                return $query;
+            }
+        }
+        function get_page(){
+            global $connect;
+            $no_of_records_per_page = 5 ;
+            $total_pages_sql = "SELECT COUNT(*)  FROM san_pham ";
+            $result = mysqli_query($connect, $total_pages_sql);
+            $total_rows = mysqli_fetch_array($result)[0];
+            $total_pages = ceil($total_rows / $no_of_records_per_page);
+            return $total_pages;
+        }
+    }
